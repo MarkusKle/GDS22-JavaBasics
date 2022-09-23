b0172caa84806adaf7df30a7667eb208fecc3144
MarkusKle <52211071@edu.campus02.at>
2022-09-23 18:29:01 +0200

    Add Class Calculator

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+
+        byte zahl1 = 5;
+        byte zahl2 = 7;
+        int ergebnis;
+        String rechnerName = "Markus";
+
+        ergebnis = zahl1 + zahl2;
+
+        System.out.println("Mein Name ist " + rechnerName + ": " + ergebnis);
+    }
+}
