c819efade6147cfd45ef816bef3485e455e0eaa1
MarkusKle <52211071@edu.campus02.at>
2022-09-23 17:10:27 +0200

    My First Class created; args as input parameter written to console

---
+++ MyFirstClass.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+
+    // wir haben unsere erste Klasse erzeugt
+
+    //psvm
+    public static void main(String[] args) {
+        // erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        // args
+        // kürzel sout + tab
+        // System.out.println(args);  // ALT + Enter auf "args"
+        System.out.println(Arrays.toString(args));
+        // args zb. ping args1 args2 args3
+        // args ping 8.8.8.8 -t
+
+
+
+
+    }
+
+}
